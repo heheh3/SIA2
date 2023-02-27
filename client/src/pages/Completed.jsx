4d86e879db1b07f69bcc3045697c3f17387a1b80
ryanlozano07 <ryan.rjl38@gmail.com>
2023-02-27 22:25:09 +0800

    Update Completed.jsx

--- client/src/pages/Completed.jsx
+++ client/src/pages/Completed.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useContext} from 'react';
+import React, {useState, useEffect} from 'react';
 import { FaSearch  } from "react-icons/fa";
 import {Link} from "react-router-dom";
 import axios from 'axios';
