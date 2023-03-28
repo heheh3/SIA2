f6b433dc7ca39fffb17366f3491de4c1fd7f4589
ryanlozano07 <ryan.rjl38@gmail.com>
2023-03-29 02:40:54 +0800

    Update MedicalHistory.jsx

--- client/src/pages/MedicalHistory.jsx
+++ client/src/pages/MedicalHistory.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import ProfileNavbar from './ProfileNavbar'
 import PatientNavbar from './PatientNavbar'
 import "../css/Profile.css";
