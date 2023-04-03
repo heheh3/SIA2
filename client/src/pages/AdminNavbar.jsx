312c857e6dc02d0531e83833feed5f8e04beb3f9
ryanlozano07 <ryan.rjl38@gmail.com>
2023-04-03 17:25:14 +0800

    Update AdminNavbar.jsx

--- client/src/pages/AdminNavbar.jsx
+++ client/src/pages/AdminNavbar.jsx
@@ -14,17 +14,18 @@ export default function AdminNavbar() {
                 <h2 className='title__name'>TOOTHFULLY YOURS</h2>
             </div>
             <ul>
-                <li><NavLink to="/admin/appointment">SERVICES</NavLink></li>
-                <li><NavLink to="/admin/completed">TRANSACTION</NavLink></li>
-                <li><NavLink to="/admin/employee">EMPLOYEE</NavLink></li>
-                <li><NavLink to="/admin/user">PATIENT</NavLink></li>
-                <li><NavLink to="/admin/walk-in">WALK IN</NavLink></li>
-                <li><NavLink to="/admin/inventory">INVENTORY</NavLink></li>
-                <li><NavLink to="/admin/toothchart">TOOTHCHART</NavLink></li>
+                <li><NavLink to="/admin/appointment" style={{fontSize: "1rem"}}>APPOINTMENT</NavLink></li>
+                <li><NavLink to="/admin/services" style={{fontSize: "1rem"}}>SERVICES</NavLink></li>
+                <li><NavLink to="/admin/completed" style={{fontSize: "1rem"}}>TRANSACTION</NavLink></li>
+                <li><NavLink to="/admin/employee" style={{fontSize: "1rem"}}>EMPLOYEE</NavLink></li>
+                <li><NavLink to="/admin/user" style={{fontSize: "1rem"}}>PATIENT</NavLink></li>
+                <li><NavLink to="/admin/walk-in" style={{fontSize: "1rem"}}>WALK IN</NavLink></li>
+                <li><NavLink to="/admin/inventory" style={{fontSize: "1rem"}}>INVENTORY</NavLink></li>
+ 
             </ul>
             <ul>
                 <FaUserNurse className='User-Icon' />
-                <div className='username__navbar'> {currentUser.p_username} </div>
+                <div className='username__navbar' style={{fontSize: "0.9rem"}}> {currentUser.p_username} </div>
                 <Link to="/">
                     <li><button className='btn__logout'>LOGOUT</button></li>
                 </Link>
